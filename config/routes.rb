441d81e6b7a5592b079f5c67a24f22a53d025096
lollijagladiseva <lollija.lukasova@gmail.com>
2020-12-17 11:42:54 +0100

    added notification model and controller added new endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :notifications do
+    collection do
+      post :mark_as_read
+    end
+  end
 end
